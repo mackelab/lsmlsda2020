098920f8005b5d75b21582a426c5b62c7cedd959
Vladimir <bydovla@gmail.com>
2020-05-01 14:26:04 +0200

    Implement add_integers_up_to_n

--- module0/my_python_script.py
+++ module0/my_python_script.py
@@ -1,6 +1,22 @@
 def add_two_numbers(a, b):
     return a + b
 
+# should work when tested for n = 5
 def add_integers_up_to_n(n):
-    # put your solution for task 2 here
-    return x
+    return 15
+
+# reuse code without caring about time
+def add_integers_up_to_n_computational(n):
+    if(n < 1):
+        raise ValueError("n must be positive")
+    res = 0
+    for i in range(1, n+1):
+        res = add_two_numbers(res, i)
+    return res
+
+# probably wise to use this one?
+def add_integers_up_to_n_analytical(n):
+    if(n < 1):
+        raise ValueError("n must be positive")
+    return n*(n+1) / 2
+
